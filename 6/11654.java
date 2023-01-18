139343be6f3f3159752b17cfb2b62567f8d99ad2
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-01-18 14:50:23 +0900

    Create 11654.java

---
+++ 6/11654.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int ch = scanner.next().charAt(0);
+    
+    System.out.println(ch);
+    scanner.close();
+  }
+}
