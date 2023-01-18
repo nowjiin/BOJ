7d9a2efcae558aa1e7ede71c8e69dc28b05b2ef3
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-01-18 13:35:45 +0900

    Update 4344.java

--- 4/4344.java
+++ 4/4344.java
@@ -52,3 +52,61 @@ public class Main {
         }
     }
 }
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+      FastReader fr = new FastReader();
+      
+      int num = fr.nextInt();
+      
+      for(int i = 0 ; i < num ; i++){
+        int n = fr.nextInt();
+        int[] arr = new int[n];
+        double sum = 0;
+
+        for(int j = 0; j < n; j++){
+          arr[j] = fr.nextInt();
+          sum += arr[j];
+        }
+        
+        double mean = (double)sum/n;
+        double count = 0;
+        
+        for(int k = 0; k < n; k++){
+          if(arr[k] > mean)
+            count ++;
+        }
+        
+        System.out.printf("%.3f%%\n", (count/n)*100);
+      }
+    }
+
+    
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
+*/
