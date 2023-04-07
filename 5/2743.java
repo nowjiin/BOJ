bdc9f3b916c0eddde6cd9b2a5183397701f86c76
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-04-07 16:10:29 +0900

    단어 길이 재기

---
+++ 5/2743.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+        System.out.println(a.length());
+				scanner.close();
+    }
+}
