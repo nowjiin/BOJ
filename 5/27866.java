0fafd05b20e668832c5191d7a46cc20471b1aaa4
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-04-07 16:07:43 +0900

    Update and rename 27866.py to 27866.java

---
+++ 5/27866.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+        int b = scanner.nextInt();
+
+        System.out.println(a.charAt(b-1));
+        scanner.close();
+    }
+}
