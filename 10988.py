050d6c036aaf2724d9cf86709d0679e89a98bdf3
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-04-15 17:41:27 +0900

    팰린드롬인지 확인하기

--- 10988.py
+++ 10988.py
@@ -1,1 +1,14 @@
+word = input()
 
+def palindrome(x):
+    left, right = 0, len(word) - 1
+    while(True):
+        if word[left] == word[right]:
+            if (left == right) or (right - left == 1):
+                return 1
+            left += 1
+            right -= 1
+        else:
+            return 0
+
+print(palindrome(word))
