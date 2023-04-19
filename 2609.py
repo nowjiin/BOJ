5251f25b055444d511259f2e809bfc53079d46ca
nowjin (HyunJin / 현진) <110535324+nowjiin@users.noreply.github.com>
2023-04-20 07:20:49 +0900

    최대공약수와 최소공배수

---
+++ 2609.py
@@ -0,0 +1,10 @@
+a,b = map(int, input().split())
+gcd = 1
+for i in range(2, max(a,b)+1):
+    while(a%i == 0 and b%i == 0):
+        a = a // i
+        b = b // i
+        gcd *= i
+
+print(gcd)
+print(gcd*a*b)
