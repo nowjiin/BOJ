0ece536e654bf721474b8186a48c320940b4cc13
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-06-24 16:47:26 +0900

    시험 성적

---
+++ 9498.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n <= 100 and n>=90:
+  print("A")
+elif n <= 89 and n >= 80:
+  print("B")
+elif n <= 79 and n>=70:
+  print("C")
+elif n <= 69 and n>=60:
+  print("D")
+else:
+  print("F")
