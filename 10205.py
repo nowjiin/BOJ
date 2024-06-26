8f79887e37d53e6ea01b451fe6662b894bac7529
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-06-26 09:23:48 +0900

    ACM 호텔

--- 10205.py
+++ 10205.py
@@ -1,1 +1,9 @@
-
+T = int(input())
+for _ in range(T):
+  H,W,N = map(int, input().split())
+  floor = N%H
+  W = N//H + 1
+  if floor == 0:
+    floor = H
+    W = N // H
+  print(f"{floor*100+W}")
