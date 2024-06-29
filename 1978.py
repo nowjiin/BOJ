03bdde6af04b74a731e01d6b3122792bcc5075a0
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-06-29 16:54:15 +0900

    소수 찾기
    로직 개선

--- 1978.py
+++ 1978.py
@@ -1,12 +1,29 @@
 n = int(input())
-a = list(map(int, input().split()))
+ary = list(map(int, input().split()))
 count = 0
 
 for i in range(n):
-    b = []
-    for j in range(1,1001):
-        if((a[i] % j) == 0):
-            b.append(j)
-    if(len(b) == 2):
-        count += 1
+    num = ary[i]
+    if num > 1:
+        is_prime = True
+        for j in range(2, int(num**0.5) + 1):
+            if num % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            count += 1
 print(count)
+
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# count = 0
+
+# for i in range(n):
+#     b = []
+#     for j in range(1,1001):
+#         if((a[i] % j) == 0):
+#             b.append(j)
+#     if(len(b) == 2):
+#         count += 1
+# print(count)
