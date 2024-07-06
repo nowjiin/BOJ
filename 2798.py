fd43ee6429bbca6fd61683fd5a63ed890234e9dc
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-07-06 18:13:27 +0900

    블랙잭

--- 2798.py
+++ 2798.py
@@ -1,3 +1,19 @@
-n,m = map(int, input().split())
+import sys
+from itertools import combinations
 
-arr = list(map(int, input().split()))
+input = sys.stdin.readline
+
+# 입력 받기
+n, m = map(int, input().strip().split())
+cards = list(map(int, input().strip().split()))
+
+answer = 0
+
+# 모든 3장의 조합을 확인
+for combo in combinations(cards, 3):
+    temp = sum(combo)
+    if temp <= m and temp > answer:
+        answer = temp
+
+# 결과 출력
+print(answer)
