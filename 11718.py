b6b90e1161b8a6709e49ad750bc48734333f5004
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-07-10 18:19:14 +0900

    그대로

--- 11718.py
+++ 11718.py
@@ -1,5 +1,14 @@
 while(1):
-	try :
-		print(input())
-	except:
-		break
+  try :
+    print(input())
+  except:
+    break
+"""-----------------------------"""
+import sys
+
+input_text = sys.stdin.read()
+
+ary = input_text.split("\n")
+
+for i in ary:
+  print(i)
