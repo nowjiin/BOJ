4fb0070dccdcabb6972d8acf00169afeae1bbf30
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-07-11 15:57:31 +0900

    별 찍기 - 7

--- 2444.py
+++ 2444.py
@@ -1,9 +1,17 @@
-N = int(input())
+n = int(input())
 
-for i in range(1, N + 1):
-	str = "*" * (2 * i - 1)
-	print(str.center(2 * N - 1).rstrip())
+for i in range(1, n + 1):
+  print(' ' * (n - i) + '*' * (2 * i - 1))
 
-for i in range(N - 1, 0, -1):
-    str = "*" * (2 * i - 1)
-    print(str.center(2 * N - 1).rstrip())
+for i in range(n - 1, 0, -1):
+  print(' ' * (n - i) + '*' * (2 * i - 1))
+
+# N = int(input())
+
+# for i in range(1, N + 1):
+#   str = "*" * (2 * i - 1)
+#   print(str.center(2 * N - 1).rstrip())
+
+# for i in range(N - 1, 0, -1):
+#   str = "*" * (2 * i - 1)
+#   print(str.center(2 * N - 1).rstrip())
