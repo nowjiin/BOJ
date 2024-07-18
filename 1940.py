e7afb9e1d73dad3cf4c18a9e9fd57268e5bf5e42
nowjin (HyunJinHwang / 황현진) <110535324+nowjiin@users.noreply.github.com>
2024-07-18 13:28:35 +0900

    주몽

--- 1940.py
+++ 1940.py
@@ -1,19 +1,23 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-count = 1
-start_index = 1
-end_index = 1
-sum = 1
+N = int(input())
+M = int(input())
+A = list(map(int, input().split()))
 
-while end_index != n:
-  if sum == n:
-    count += 1
-    end_index+=1
-    sum +=end_index
-  elif sum > n:
-      sum -= start_index
-      start_index += 1
-  else:
-      sum += end_index
+A.sort()
+count = 0
+i = 0
+j = N-1
 
+while i<j:
+    if A[i] + A[j] < M:
+        i+=1
+    elif A[i] + A[j] >M:
+        j-= 1
+    else:
+        count += 1
+        i += 1
+        j -= 1
+        
 print(count)
