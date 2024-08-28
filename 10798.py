1d8e2d3bc73f2624139aa9dcee31e9765a0bb26f
nowjin (HyunJinHwang / 황현진) <hyunjin117@naver.com>
2024-08-28 22:21:08 +0900

    세로읽기

--- 10798.py
+++ 10798.py
@@ -1,1 +1,12 @@
- #  세로읽기
+words = [input() for _ in range(5)]
+
+max_length = max(len(word) for word in words)
+
+result = []
+
+for i in range(max_length):
+    for word in words:
+        if i < len(word):
+            result.append(word[i])
+
+print(''.join(result))
