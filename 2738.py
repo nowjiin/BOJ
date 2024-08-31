8ae6dc66b048f72f3f1eac373f2a0f175f5869ad
nowjin (HyunJinHwang / 황현진) <hyunjin117@naver.com>
2024-08-31 21:47:27 +0900

    행렬 덧셈

--- 2738.py
+++ 2738.py
@@ -1,1 +1,21 @@
+n, m = map(int, input().split())
 
+A = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+B = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    B.append(row)
+
+result = []
+for i in range(n):
+    result_row = []
+    for j in range(m):
+        result_row.append(A[i][j] + B[i][j])
+    result.append(result_row)
+
+for row in result:
+    print(' '.join(map(str, row)))
