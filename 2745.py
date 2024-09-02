e96911fd49863fd5c9a7cbf1f88d81bba3865d2a
nowjin (HyunJinHwang / 황현진) <hyunjin117@naver.com>
2024-09-02 23:27:36 +0900

    진법

--- 2745.py
+++ 2745.py
@@ -1,19 +1,6 @@
-import sys
-input = sys.stdin.read
-data = input().split()
+N, B = input().split()
+B = int(B)
 
-N = data[0]
-B = int(data[1])
+result = int(N, B)
 
-decimal_value = 0
-power = 1
-
-for char in reversed(N):
-    if '0' <= char <= '9':
-        value = ord(char) - ord('0')
-    else:
-        value = ord(char) - ord('A') + 10
-    decimal_value += value * power
-    power *= B
-
-print(decimal_value)
+print(result)
