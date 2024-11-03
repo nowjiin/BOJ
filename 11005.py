fab02d66b5bbc4ceed8f40b54f7218f54447bd83
nowjin (HyunJinHwang / 황현진) <hyunjin117@naver.com>
2024-11-03 20:25:53 +0900

    진법 변환 2

--- 11005.py
+++ 11005.py
@@ -1,13 +1,17 @@
+# 진법 변환2
+"""
+10진법 수 N을 B진법으로 출력
+"""
+
 N, B = map(int, input().split())
 
-result = []
+answer = ""
+digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-while N > 0:
+while N>0:
     remainder = N % B
-    if remainder >= 10:
-        result.append(chr(remainder - 10 + ord('A')))
-    else:
-        result.append(str(remainder))
+    answer = digits[remainder] + answer
     N //= B
 
-print(''.join(result[::-1]))
+print(answer)
+
